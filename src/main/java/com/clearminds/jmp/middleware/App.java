8f51d098e3dd056e9625f954a2d4eb2f77ef78da
Jonathan Morocho <jmorocho35@gmail.com>
2020-12-09 09:35:13 -0500

    Version Inicial

---
+++ src/main/java/com/clearminds/jmp/middleware/App.java
@@ -0,0 +1,13 @@
+package com.clearminds.jmp.middleware;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
